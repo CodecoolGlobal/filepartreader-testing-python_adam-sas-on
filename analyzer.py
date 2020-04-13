e9a78256b54a9789cb78d5bd7de09f4b0dc27996
Adam Sz <52573736+adam-sas-on@users.noreply.github.com>
2020-04-13 19:01:23 +0200

    Add fields into class

--- analyzer.py
+++ analyzer.py
@@ -1,8 +1,16 @@
 from reader import FilePartReader
+import re
 
 
 class FileAnalyzer:
     def __init__(self, reader: FilePartReader):
         self.reader = reader
+        self.split_regex = re.compile("[^\w\-']+[^\w]*")
+        # sets if e.g. 'A' is the same what 'a' or not. Default not:
+        self.case_sensitive = True
+        # sets if analyzed words can be repeated or not. Default not:
+        self.read_unique = True
 
     # TODO
+
+# list = str.splitlines()
